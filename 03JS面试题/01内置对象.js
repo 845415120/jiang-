851e3207df0b4d723d43e1503c682bc706d7aedf
江鹏利 <845415120@qq.com>
2023-04-08 21:45:02 +0800

    提交

---
+++ 03JS面试题/01内置对象.js
@@ -0,0 +1,20 @@
+// 数据类
+Object
+Array
+Boolean
+Number
+String
+// 其他对象
+Function
+Argument //类数组
+Math
+Date
+RegExp
+Error //在错误发生时提供了错误的提示信息。
+//ES6新增对象
+Symbol 
+Map
+Set
+Promises
+Proxy
+Reflect  //提供拦截JavaScript操作的方法
